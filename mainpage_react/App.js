82fa5c60a71aac58bb3b5d905a502ce3c5aa49aa
planetyoon <planetyoon@naver.com>
2022-05-02 19:06:27 +0900

    메인페이지

---
+++ mainpage_react/App.js
@@ -0,0 +1,20 @@
+//import { render } from '@testing-library/react';//
+import React, { Component } from 'react';
+
+import './App.css';
+
+
+class App extends Component {
+  
+ render() { 
+   
+   return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+
+export default App;
