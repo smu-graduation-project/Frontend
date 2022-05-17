7fda9f0578c62aaa2a759cadc1e33acaac73d8ae
Kim Ju Eun <wndms2074@naver.com>
2022-05-17 21:50:12 +0900

    노드세부페이지-a.js 서버구현 성공

--- mainpage_react/src/components/Node/Chart_test.js
+++ mainpage_react/src/components/Node/Chart_test.js
@@ -1,73 +1,54 @@
-import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
-import { AreaChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import { AreaChart, Line, XAxis, YAxis, Label, ResponsiveContainer, Area, Tooltip, CartesianGrid } from 'recharts';
 import Title from './Title';
 
-function createData(time, amount) {
-    return { time, amount };
-  }
-  
-  const data = [
-    createData('00:00', 0),
-    createData('03:00', 300),
-    createData('06:00', 600),
-    createData('09:00', 800),
-    createData('12:00', 1500),
-    createData('15:00', 2000),
-    createData('18:00', 2400),
-    createData('21:00', 2400),
-    createData('24:00', undefined),
-  ];
-  
-  export default function Chart() {
-    const theme = useTheme();
+import axios from 'axios';
+import React, { useEffect, useState} from 'react';
+
+var port=1;
+
+const ChartTemp = ({}) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://220.149.31.104:9090/api/formerData/temperature/list"+"/"+port)
+      .then((res) => {
+        const dataTemp = res.map((data) => {
+          <li key={data.date} ></li>
+        });
+      setData([{}, ...dataTemp, {}]);
+    });
+  });
   
     return (
       <React.Fragment>
         <Title>Today</Title>
-        <ResponsiveContainer>
-          <AreaChart
-            data={data}
-            margin={{
-              top: 16,
-              right: 16,
-              bottom: 0,
-              left: 24,
-            }}
-          >
-            <XAxis
-              dataKey="time"
-              stroke={theme.palette.text.secondary}
-              style={theme.typography.body2}
-            />
-            <YAxis
-              stroke={theme.palette.text.secondary}
-              style={theme.typography.body2}
-            >
-              <Label
-                angle={270}
-                position="left"
-                style={{
-                  textAnchor: 'middle',
-                  fill: theme.palette.text.primary,
-                  ...theme.typography.body1,
-                }}
-              >
-                Temperature
-
-              </Label>
-            </YAxis>
-            <Line
-              isAnimationActive={false}
-              type="monotone"
-              dataKey="amount"
-              stroke={theme.palette.primary.main}
-              dot={false}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+        <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={setData}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="xAxis" />
+          <YAxis yAxisId="left" />
+          <YAxis yAxisId="right" orientation="right" />
+          <Tooltip />
+          <Area type="monotone" dataKey="min" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="average" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+          <Area type="monotone" dataKey="max" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        </AreaChart>
+      </ResponsiveContainer>
       </React.Fragment>
-    );
-  }
+    )
+  
+}
 
+export default ChartTemp;
   
--- mainpage_react/src/components/Node/a.js
+++ mainpage_react/src/components/Node/a.js
@@ -1,29 +1,52 @@
 import axios from 'axios';
 import React, { useEffect, useState} from 'react';
-var port = 1;
+import qs from 'qs';
 
-const DataRequest = (props) => {
-    const [data, setData] = useState('전송실패');
+var port=1;
+
+const DataRequest = () => {
+    const [datas, setDatas] = useState([]);
+    
+
     useEffect(() => { 
         axios({
         method: "get", // 통신 방식
-        url: "http://220.149.31.104:9090/api/formerData/temperature/list"+"/"+":port", // 서버
+        url: "http://220.149.31.104:9090/api/formerData/temperature/list"+"/"+port, // 서버
         params: {
-            "startDate" : "2022-01-01",
-            "endDate" : "2022-03-05",
-            }
+            startDate : "2022-01-01",
+            endDate : "2022-03-06"
+        },
+        paramsSerializer: function (params) {
+            return qs.stringify(params, {arrayFormat: 'brackets'})
+        },
+        responseType: 'json',
+        headers: {'Content-Type': `application/json`},
         })
-        .then(function(response) {
-            setData(response.data);
-            console.log(response.success);
+
+// axios 정의 마침.
+// axios.then 시작.
+        .then(res => {
+            console.log(res.data); // [{}, ... ,{}] 형태
+            return res.data;
+        })
+        .then(data => {
+            setDatas(JSON.stringify(data));
+            
+            // console.log(response.success)
             // console.log(response.status)
             // console.log(response.statusText)
             // console.log(response.headers)
             // console.log(response.config)
         })
-    });
+    },[]);
+
     return(
-        <h1> {data} </h1>
+        <>
+        {/* 출력테스트1 */}
+        <h1> {datas} </h1>
+
+        
+        </>
     );
 
 }
